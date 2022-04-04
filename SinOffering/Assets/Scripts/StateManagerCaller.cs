e6f2caad70247397cf9c92bb2b5569ab1ad1336b
Joshua <gallopingbull89@gmail.com>
2022-04-04 03:08:16 -0700

    fixed animation event issue with idle animation for bloody sprite

--- SinOffering/Assets/Scripts/StateManagerCaller.cs
+++ SinOffering/Assets/Scripts/StateManagerCaller.cs
@@ -1,12 +1,11 @@
-/**
- * helper class to StateManager.cs for animation events
- */
 using UnityEngine;
+
+/// <summary>
+/// helper class to StateManager.cs for notifications for animation events
+/// </summary>
 
 public class StateManagerCaller : MonoBehaviour
 {
-    public void CallStateEnter(Entity.State state)
-    {
+    public void CallStateEnter(Entity.State state) =>
         PlayerController.instance.StateManager.EnterState(state);
-    }
 }
